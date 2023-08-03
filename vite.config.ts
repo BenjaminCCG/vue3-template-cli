83a65543142ddc3da8dbed585c15a1443423afaf
BenjaminCCG <42145150+BenjaminCCG@users.noreply.github.com>
2023-08-03 17:37:27 +0800

    feat(组件): add

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,51 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import checker from 'vite-plugin-checker';
 import WindiCSS from 'vite-plugin-windicss';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    WindiCSS(),
-    checker({
-      // vueTsc: true,
-      eslint: {
-        lintCommand: 'eslint "./src/**/*.{ts,tsx,vue,js,jsx}"'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, './env');
+  return {
+    plugins: [
+      vue(),
+      WindiCSS(),
+      AutoImport({
+        imports: ['vue', 'vue-router'],
+        resolvers: [ElementPlusResolver()],
+        dts: './src/auto-imports.d.ts',
+        eslintrc: {
+          enabled: true
+        }
+      }),
+      Components({ resolvers: [ElementPlusResolver()] }),
+      checker({
+        // vueTsc: true,
+        eslint: {
+          lintCommand: 'eslint "./src/**/*.{ts,tsx,vue,js,jsx}"'
+        }
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
       }
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
+    },
+    server: {
+      host: '0.0.0.0',
+      port: Number(env.VITE_APP_PORT),
+      open: true,
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: env.VITE_APP_BASE_API,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(new RegExp(`^${env.VITE_APP_BASE_API}`), '')
+        }
+      }
     }
-  }
+  };
 });
