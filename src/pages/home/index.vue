aa09a0b1f2b2621e94ee75c617c8c409e849b2ec
benjamin <332935650@qq.com>
2023-08-04 21:17:07 +0800

    feat(组件): 调整

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,12 +1,8 @@
 <template>
   <div>hello world,{{ userStore.name }}</div>
-  {{ a }}
-  <HelloWorld msg="hhh" />
 </template>
 
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue';
 import { useUserStore } from '@/store/user';
 const userStore = useUserStore();
-const a = ref(1);
 </script>
